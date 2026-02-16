24ed06efd480563ba2b2f53f8864cef31c901b55
Ahmad Haileh <ahmad@myshoefitter.com>
2026-02-16 16:24:45 +0100

    Update static IP to 192.168.95.6, gateway 192.168.95.2

--- scripts/set_static_ip.sh
+++ scripts/set_static_ip.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# Set static IP 192.168.95.2 on enp2s0 via NetworkManager
-nmcli con mod "Wired connection 1" ipv4.method manual ipv4.addresses 192.168.95.2/24 ipv4.gateway 192.168.95.1 ipv4.dns "8.8.8.8 8.8.4.4"
+# Set static IP 192.168.95.6 on enp2s0 via NetworkManager
+# Gateway: 192.168.95.2, Subnet: 255.255.255.0 (/24)
+nmcli con mod "Wired connection 1" ipv4.method manual ipv4.addresses 192.168.95.6/24 ipv4.gateway 192.168.95.2 ipv4.dns "8.8.8.8 8.8.4.4"
 echo "Config saved, reactivating..."
 nmcli con up "Wired connection 1"
-echo "Static IP set to 192.168.95.2"
+echo "Static IP set to 192.168.95.6 (gw 192.168.95.2)"
